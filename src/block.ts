837b425ed06c5fe37c2c77c22be94e5d14646856
Likem <likemdzokoto@gmail.com>
2022-09-06 04:01:26 +0000

    defined constructor parameters

--- src/block.ts
+++ src/block.ts
@@ -1,9 +1,22 @@
 interface BlockData{
      index : number;
      hash : string;
-     PreviousHash: string;
+     previoushash: string;
      nonce : number;
      transactions: any[];
      key: string;
 
+}
+
+export default class Block implements BlockData{
+
+    //define a constructor for the class
+    constructor(
+        public index : number = 0,
+        public hash : string = '',
+        public previoushash : string = '',
+        public nonce : number =0,
+        public transaction : any[] = [],
+
+    ){}
 }

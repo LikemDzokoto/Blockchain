df5724a0050f2212eb0f61af40215b09abe72ff4
LikemDzokoto <likemdzokoto@gmail.com>
2022-09-28 12:18:26 +0000

    feat: setup api server

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,44 @@
+import express from 'express';
+import * as bodyParser from 'body-parser'
+
 import Blockchain from "./blockchain";
 import Block from "./block";
 import Transaction from "./transaction";
+import { TransactionData } from './types/class';
+// import { BlockData } from '../types/types/class';
 
-let b = new  Block();
+
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+//create a genesis block 
+let genesisBlock = new Block();
+
+
 
 //intialise the blockchain with the genesis block
-let bc = new Blockchain(b);
-
-//create a transaction
-let t = new Transaction('me','you', 7);
+let bc = new Blockchain(genesisBlock);
 
 
-//mining phase
-let newb = bc.getNextBlock([t]);
-bc.addBlock(newb)
+app.get('/', function(req, res){
+    res.json(bc.blocks)
+    
+})
 
-console.log(bc.blocks[1].transactions);
+// //create a transaction
+// let t = new Transaction('me','you', 7);
+let transactions: TransactionData[] = [];
+
+
+// //mining phase
+// let newb = bc.getNextBlock([t]);
+// bc.addBlock(newb)
+
+// console.log(bc.blocks[1].transactions);
+
+app.listen(3000, function(){
+    console.log('port has started on 3000')
+})
